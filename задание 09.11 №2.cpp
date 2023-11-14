54f1c2fc3f6199133c5beb28d5b7eb9e9df95437
DIREKTOR228PLAY <150294748+DIREKTOR228PLAY@users.noreply.github.com>
2023-11-14 09:36:40 +0300

    Add files via upload

---
+++ задание 09.11 №2.cpp
@@ -0,0 +1,42 @@
+﻿// задание 09.11 №2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>;
+#include <cmath>;
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    int decimalNumber;
+
+    cout << "Введите десятичное число: ";
+    cin >> decimalNumber;
+
+    if (decimalNumber < 0) {
+        cout << "Отрицательные числа не поддерживаются." << endl;
+        return 1;
+    }
+
+    if (decimalNumber == 0) {
+        cout << "Двоичное представление: 0" << endl;
+        return 0;
+    }
+
+    int binary[255];
+    int index = 0;
+
+
+    while (decimalNumber > 0) {
+        binary[index] = decimalNumber % 2;
+        decimalNumber /= 2;
+        index++;
+    }
+
+    cout << "Двоичное представление: ";
+    for (int i = index - 1; i >= 0; i--) {
+        cout << binary[i];
+    }
+    cout << std::endl;
+
+    return 0;
+}
